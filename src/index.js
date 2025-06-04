1ed6f51bcd276bf97f1cb546df8cb206d29a5ab8
keta-dev <89269904+keta-dev@users.noreply.github.com>
2025-06-04 13:25:32 +0100

    AI API source added

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "07678d78bd5438a79f8d4tc414d2o39e";
-  let context = "You are an African folktale Poem expert and love to write short poems. You mission is to generate a 5 line poem written in the local dialect in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+  let context =
+    "You are an African folktale Poem expert and love to write short poems. You mission is to generate a 5 line poem written in the local dialect in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate African poems about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="generating">⏳ Generating Africa poems about ${instructionsInput.value}</div>`;
-
 
   axios.get(apiUrl).then(showPoem);
 }
